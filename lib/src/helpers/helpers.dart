454cde6a4de203736084a6e706f895f0755a6784
Andres Badillo <r.andres.badillo@gmail.com>
2022-11-22 09:29:16 -0500

    First commit

---
+++ lib/src/helpers/helpers.dart
@@ -0,0 +1,31 @@
+List<String> getLyrics() {
+  return [
+    'Ven, quiero un poco más',
+    'salir de la realidad',
+    'de ti me quiero embriagar.',
+    'Ven, ven y calma esta ansiedad',
+    'ilumina mi oscuridad',
+    'y mis labios encontraras.',
+    'Cada segundo te acercaras',
+    'y nuestros cuerpos se fundirán',
+    'y las palabras no bastarán',
+    'para amarte y tomarte hoy.',
+    'Hoy serás',
+    'mi sueño eterno',
+    'no quiero despertar',
+    'te llevare',
+    'donde nadie nos pueda encontrar',
+    'donde nadie nos pueda...',
+    '...',
+    'Cada segundo te acercaras',
+    'y nuestros cuerpos se fundirán',
+    'y las palabras no bastarán',
+    'para amarte y tomarte hoy.',
+    'Hoy serás',
+    'mi sueño eterno',
+    'no quiero despertar',
+    'te llevare',
+    'donde nadie nos pueda encontrar',
+    'donde nadie nos pueda…'
+  ];
+}
