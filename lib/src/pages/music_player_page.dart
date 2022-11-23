4b4b8259b36f954098e2f351629aaf8b24d196f3
Andres Badillo <r.andres.badillo@gmail.com>
2022-11-22 20:18:43 -0500

    Se termino el diseño principal

--- lib/src/pages/music_player_page.dart
+++ lib/src/pages/music_player_page.dart
@@ -1,14 +1,252 @@
 import 'package:flutter/material.dart';
 
+import 'package:music_player/src/helpers/helpers.dart';
+import 'package:music_player/src/widgets/custom_appbar.dart';
+
 class MusicPlayerPage extends StatelessWidget {
   const MusicPlayerPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('MusicPlayerPage'),
+    return Scaffold(
+      body: Stack(
+        children: [
+          Background(),
+          Column(
+            children: [
+              CustomAppBar(),
+              ImgDiscoDuracion(),
+              TituloPlay(),
+              Expanded(
+                child: Lyrics(),
+              ),
+            ],
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
+    return Container(
+      width: double.infinity,
+      height: screenSize.height * 0.8,
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xff33333E),
+              Color(0xff201E28),
+            ],
+            begin: Alignment.centerLeft,
+            end: Alignment.center,
+          ),
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(60),
+            bottomRight: Radius.circular(60),
+          )),
+    );
+  }
+}
+
+class Lyrics extends StatelessWidget {
+  const Lyrics({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final lyrics = getLyrics();
+
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: ListWheelScrollView(
+        physics: const BouncingScrollPhysics(),
+        itemExtent: 42,
+        diameterRatio: 1.8,
+        children: lyrics
+            .map((linea) => Text(
+                  linea,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white.withOpacity(0.8),
+                  ),
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
+
+class TituloPlay extends StatelessWidget {
+  const TituloPlay({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      margin: const EdgeInsets.only(top: 40),
+      child: Row(
+        children: [
+          Column(
+            children: [
+              Container(
+                alignment: Alignment.center,
+                width: 250,
+                child: Text(
+                  'Donde nadie nos pueda encontrar',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white.withOpacity(0.8),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                alignment: Alignment.center,
+                width: 250,
+                child: Text(
+                  '- DRÜX -',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white.withOpacity(0.5),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const Spacer(),
+          FloatingActionButton(
+            elevation: 0,
+            highlightElevation: 0,
+            onPressed: () {},
+            backgroundColor: const Color(0xffF8CB51),
+            child: const Icon(Icons.play_arrow, size: 40),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ImgDiscoDuracion extends StatelessWidget {
+  const ImgDiscoDuracion({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      margin: const EdgeInsets.only(top: 70),
+      child: Row(
+        children: [
+          ImagenDisco(),
+          SizedBox(width: 30),
+          BarraProgreso(),
+          Spacer(),
+        ],
+      ),
+    );
+  }
+}
+
+class BarraProgreso extends StatelessWidget {
+  const BarraProgreso({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final estilo = TextStyle(color: Colors.white.withOpacity(0.4));
+
+    return Container(
+      child: Column(
+        children: [
+          Text('00:00', style: estilo),
+          const SizedBox(height: 10),
+          Stack(
+            children: [
+              Container(
+                width: 3,
+                height: 230,
+                color: Colors.white.withOpacity(0.1),
+              ),
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  width: 3,
+                  height: 150,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Text('00:00', style: estilo),
+        ],
+      ),
+    );
+  }
+}
+
+class ImagenDisco extends StatelessWidget {
+  const ImagenDisco({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      width: 250,
+      height: 250,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(200),
+        gradient: const LinearGradient(
+          begin: Alignment.topLeft,
+          colors: [
+            Color(0xff484750),
+            Color(0xff1E1C24),
+          ],
+        ),
+      ),
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(200),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              const Image(image: AssetImage('assets/dnnpe-2.jpg')),
+              Container(
+                width: 25,
+                height: 25,
+                decoration: BoxDecoration(
+                  color: Colors.black38,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+              ),
+              Container(
+                width: 18,
+                height: 18,
+                decoration: BoxDecoration(
+                  color: const Color(0xff1C1C25),
+                  borderRadius: BorderRadius.circular(100),
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
